f65341423898913fe91230a3941405452d976636
LuisL <luislunandroid@gmail.com>
2022-11-29 22:54:04 +0000

    version finalizada, solo falta incluir el Handwrite font en el ul y acomodar el interlineado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,58 +1,38 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useState } from "react";
+ 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [list,setList]=useState([])
-	
-	
-	//const [check, setCheck]=useState(false)
-	const borrar =(indice)=>()
-	const aux = {list,{"label","done"}}
-		console.log(aux)
-	}
-	const tareas =(e)=>{setList([...list,{"label":e ,"done":false}])
-		}
-	const getTask =()=>{
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/bairon00")
-		.then(data=>data.json())
-		.then(response=>setList(response))
-	}
-	const putTask=()=>{
-		let header= new Headers();
-		header.append("content-Type","application/json")
-		let cuerpo=JSON.stringify(
-			list
-			);
-		let requesOptions={
-			method:"PUT",
-			headers:header,
-			body:cuerpo,
-			redirect:"follow"
-		};
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/bairon00",requesOptions)
-		.then(data=>data.json())
-		.then(response=>console.log(response))
-	};
-useEffect(()=>{
-	getTask();
+const [lista, setLista] = useState([])
+const norefresh =(evento) => {evento.preventDefault();
+console.log(evento.target[0].value)
+setLista([... lista,evento.target[0].value])
+}
+const DeleteItems = (indexItem) => {
+    setLista((prevState) =>
+      prevState.filter((elemento, indice) => indice !== indexItem)
+    );
+  };
 
-},[]);
-
-		return(
-		<div className="text-center">
-			<form action="submit" onSubmit={(e)=>{e.preventDefault();
-			    tareas(e.target[0].value);
-				putTask();
-			}}>
-			<h1 className="text-center mt-5">tareas</h1>
-		</form>
-		<input type="text" placeholder="TAREAS" />
-		{list.map((elm,indice)=>{if(elm.done===false)
-		return <li key={indice}>{elm.label} <button onClick={()=>borrar(indice)}>x</button></li>})}
+	return (
+		<div  className="container position-relative">
+			<img src="https://imagizer.imageshack.com/img923/3784/tgL04X.png" className="agenda position-absolute"></img>
+			<form type="submit" onSubmit={norefresh}>
+			{/* <p> Tareas:</p> */}
+			<input type="text" className="position-relative" placeholder="type down your task"></input>
+			</form>
+			<ul>
+				{lista.map((elemento, indice)=>{
+					return <li key={indice}>{elemento} <button className="btn" onClick={() => DeleteItems(indice)}>
+					<i className="fas fa-trash-alt" />
+				  </button></li>
+				})}
+			</ul>
 		</div>
+
 	);
+};
 
 export default Home;
